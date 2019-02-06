8176e700bdd85e89fbf13220399a9f95c860e32d
HenryPark <parkh21@wwu.edu>
2019-02-05 18:25:14 -0800

    Clean and seperate the code.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This project contains a single HTML file that is a functional Jeopardy template. Features include ability to handle multiple teams and score tracking. This is a modified version of a template originally developed by jeopardylabs.com.
 
 Template was developed for Science Masters, a science outreach program at the University of Utah School of Medicine geared towards junior high school students.
+
+THis updated version cleans out single files into a folder structure and adds the ability to quickly create your own Jeopardy by reading from a text file. 
---
+++ js/jeopardy.js
@@ -0,0 +1,76 @@
+var game = {}
+game.init = function()
+{
+    $('#game').fadeIn(1000);
+    $('#options').hide()
+    $('#stats').show()
+    game.team_cnt = $('#teams').val()
+    game.createScoreboard()
+        game.current_points = 0;
+}
+
+game.createScoreboard = function()
+{
+    var content = "<table cellspacing=10><tbody><tr>";
+    for(var i = 1; i <= game.team_cnt; i++)
+    {
+        content += "<th><input class='team-name' type='text' value='Team " + i + "' /></th>";
+    }
+    content += "</tr><tr>";
+    for(var i = 1; i <= game.team_cnt; i++)
+    {
+        //content += "<td><h3 id='team" + i +"'>0</h3><span class='add-points' onclick='addPoints(" + i +  ")'>+</span> <span class='remove-points' onclick='removePoints(" + i +  ")'>-</span></td>";
+        content += "<td><h3 id='team" + i +"'>0</h3><input class='add-points' onclick='game.addPoints(" + i +  ")' value='+' type='button' /> <input class='subtract-points' onclick='game.subtractPoints(" + i +  ")' type='button' value='-' /></td>";
+    }
+    content += "</tr></tbody></table>";
+    $('#stats').html(content);
+
+}
+
+game.addPoints = function(team)
+{	
+    var points = parseInt($('#team' + team).html()) + game.current_points;
+    
+    $('#team' + team).html(points);
+    $(('#t' + game.current_questionID)).addClass("dirty");
+
+    $(('#t' + game.current_questionID)).unbind('mouseover')
+    $(('#t' + game.current_questionID)).unbind('mouseout')	
+}
+
+game.subtractPoints = function(team)
+{
+    var points = parseInt($('#team' + team).html()) - game.current_points;
+    
+    $('#team' + team).html(points);
+    $(('#t' + game.current_questionID)).addClass("dirty");	
+    $(('#t' + game.current_questionID)).unbind('mouseover')
+    $(('#t' + game.current_questionID)).unbind('mouseout')	
+}
+
+var prompt = {}
+prompt.show = function(questionID, points)
+{
+    game.current_points = points
+    game.current_questionID = questionID
+    $('#question').hide()
+    $('#game').hide()
+    $('#prompt').fadeIn(1000)
+    $('#question').html($('#' + questionID).html())
+    $('#answer').html($('#a' + questionID).html())
+    if($('#question').html().length == 0)
+        $('#correct-response').hide()
+    else
+        $('#correct-response').show();
+}
+
+prompt.hide = function()
+{
+    $('#prompt').hide();
+    $('#game').show();
+}
+
+prompt.showQuestion = function()
+{
+    $('#question').fadeIn(1000)
+}
